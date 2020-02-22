0c9bd0dce367dcfd35f34f96c058e73c5cb8a5ef
radex02 <jmarineau01@gmail.com>
2020-02-22 12:08:23 -0500

    1 file modified

--- index.ts
+++ index.ts
@@ -8,24 +8,16 @@ const app = new PIXI.Application({
 document.body.appendChild(app.view);
 
 //style
-/*
 app.renderer.view.style.position = "absolute";
 app.renderer.view.style.display = "block";
 app.renderer.resize(window.innerWidth, window.innerHeight);
 window.addEventListener("resize", function(){
   app.renderer.resize(window.innerWidth, window.innerHeight);
 });
-*/
+
 //caching
-
-
-//const texture = PIXI.Texture.from("./images/doc.png");
-//const doc = new PIXI.Sprite(texture);
-const doc = PIXI.Sprite.from("images/doc.png");
+const texture = PIXI.Texture.from("images/doc.png");
+const doc = new PIXI.Sprite(texture);
 
 //action
-doc.anchor.set(0.5);
-doc.x = app.screen.width / 2;
-doc.y = app.screen.height / 2;
-
 app.stage.addChild(doc);
