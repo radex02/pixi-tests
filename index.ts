13969cc0615015cbb86e18f62b8df0a15d1683fa
radex02 <jmarineau01@gmail.com>
2020-02-25 15:51:56 -0500

    Shit was made

--- index.ts
+++ index.ts
@@ -1,44 +1,46 @@
 import './style.css';
 import * as PIXI from 'pixi.js';
 
-//init
+//create container
 const app = new PIXI.Application({
   backgroundColor: 0x70fdff,
   resolution: 1
 });
 document.body.appendChild(app.view);
 
-//style
+//initiating style vars
 let viewport:string;
-let long:number = Math.max(window.innerWidth, window.innerHeight);
-let large:number = Math.min(window.innerWidth, window.innerHeight);
+let long:number;
+let large:number;
+let name:string;
+let property:string;
 
-if (window.innerWidth < window.innerHeight) viewport = "portrait";
-else viewport = "landscape";
+//style
 app.renderer.view.style.position = "absolute";
 app.renderer.view.style.display = "block";
-app.renderer.resize(window.innerWidth, window.innerHeight);
 
-window.addEventListener("resize", function(){
-  app.renderer.resize(window.innerWidth, window.innerHeight);
-  long = Math.max(window.innerWidth, window.innerHeight);
-  large= Math.min(window.innerWidth, window.innerHeight);
+function initStyle() {
   if (window.innerWidth < window.innerHeight) viewport = "portrait";
   else viewport = "landscape";
-  let name:string;
-  let property:string;
+  long = Math.max(window.innerWidth, window.innerHeight);
+  large= Math.min(window.innerWidth, window.innerHeight);
+  app.renderer.resize(window.innerWidth, window.innerHeight);
+}
+initStyle()
+
+//when resize
+window.addEventListener("resize", function(){
+  initStyle()
   for (name in environment.cores) {
     for (property in environment.cores[name]) {
       if (typeof environment.cores[name][property] === "function") {
         environment.sprites[name][property] = environment.cores[name][property]() 
-      } else {
-        environment.sprites[name][property] = environment.cores[name][property]
       }
     }
   }
 });
 
-//caching
+//creating environment
 const environment = {
   sprites: {},
   cores: {}
@@ -50,6 +52,9 @@ function initSprite(name:string, source:string, init:{}) {
       environment.sprites[name] = new PIXI.Sprite(app.loader.resources[name].texture);
       environment.cores[name] = init;
       environment.sprites[name].anchor.set(0.5);
+      
+      //setting properties
+      environment.cores[name].me = environment.sprites[name]; //setting reference
       let property;
       for (property in init) {
         if (typeof environment.cores[name][property] === "function") {
@@ -68,11 +73,16 @@ initSprite("doc", "https://raw.githubusercontent.com/radex02/pixi-tests/master/i
   y() {return long*0.075 + 10},
   width() {return long*0.15},
   height() {return long*0.15},
-  roundPixels: true
+  ticker: function(delta:number){
+    this.me.rotation += 0.06;
+  }
 });
 
+startup()
 function startup() {
   app.ticker.add(function(delta) {
-      
+    for (name in environment.cores) {
+      if (typeof environment.cores[name].ticker === "function") environment.cores[name].ticker(delta)
+    }
   })
 }
